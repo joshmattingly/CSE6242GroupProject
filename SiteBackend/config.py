e06db994d93e3e8368bca41bfb09f61b06deb2f6
Josh Mattingly <josh.mattingly@conagra.com>
2019-04-09 13:17:36 -0500

    map

--- SiteBackend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
-
-api_key = 'AIzaSyCLKuAgHOU70gyFXOl-pLwvyoDbXqU6rxI'
